0f97e58dacd9d2c67ca8b81874f2a32dfc39355c
gkibria <gkibria121@gmail.com>
2024-11-16 21:49:12 +0600

    allow unitech domain in cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000', 'http://192.168.0.6:3000'],
+    'allowed_origins' => ['http://localhost:3000', 'http://192.168.0.6:3000','https://www.unitechholdingsltd.com'],
 
     'allowed_origins_patterns' => [],
 
